2cf0b79bcca6d6b16da56946a180acbb7cc327b8
hyperboli <akulov.exe@gmail.com>
2025-05-05 02:01:10 +0300

    I added the use of the application's start parameters for a more informative display in swaggers.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+
+app = FastAPI(
+    redoc_url="/redoc_test",
+    docs_url="/docs",
+    openapi_url="/openapi.json",
+    title="car rental",
+    version="1.0.0",
+    description="Application for convenient car rental.",
+    contact={
+        "name" : "Alexandr Akulov",
+        "email": "akulov.am@phystech.edu",
+    },
+    )
 
 
 @app.get("/")
