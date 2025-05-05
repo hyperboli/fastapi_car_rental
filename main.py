487541cb4e455e0be9092480ca3cff184b751f54
hyperboli <akulov.exe@gmail.com>
2025-05-05 04:42:47 +0300

    I cleaned up everything unnecessary. I started the project anew.

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
 from fastapi import FastAPI
 
 
-app = FastAPI(
-    redoc_url="/redoc_test",
-    docs_url="/docs",
-    openapi_url="/openapi.json",
-    title="car rental",
-    version="1.0.0",
-    description="Application for convenient car rental.",
-    contact={
-        "name" : "Alexandr Akulov",
-        "email": "akulov.am@phystech.edu",
-    },
-    )
+app = FastAPI(title="Car Rental System")
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
